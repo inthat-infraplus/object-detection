f628a114d6a84bba13bdf2e657b5d799fe7c474f
inthat-infraplus <inthat.atwork@gmail.com>
2025-04-22 10:31:21 +0700

    Update people counter.py

--- Project2 - People counter/people counter.py
+++ Project2 - People counter/people counter.py
@@ -7,7 +7,7 @@ from sort import *
 import numpy as np
 
 #cap = cv2.VideoCapture(0)  # laptop camera
-cap = cv2.VideoCapture("../Videos/road4.mp4")  # for video
+cap = cv2.VideoCapture("../Videos/esca1.mp4")  # for video
 #cap.set(3,1280)
 #cap.set(4,720)
 
@@ -93,12 +93,18 @@ classNames = {0: 'person',
  78: 'hair drier',
  79: 'toothbrush'}
 
-mask = cv2.imread("mask.png")  # Load the mask image
+mask = cv2.imread("esca mask.png")  # Load the mask image
 
 #tracking
 tracker = Sort(max_age=20,min_hits=3,iou_threshold=0.3)
-limit = [800,400,1500,400]
+#limit = [1100,500,1370 ,600]
+limit = [1100,650,1420 ,900]  
 totalCount = []
+
+limitsUp = [1100,500,1370 ,600]
+limitsDown = [1100,650,1420 ,900]  
+totalCountUp = []
+totalCountDown = []
 
 while True:
     success,img = cap.read()  # Read a frame from the camera
@@ -113,7 +119,6 @@ while True:
             #Bounding Box
             x1,y1,x2,y2 = box.xyxy[0]
             x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)  # Convert coordinates to integers
-            #cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)  # Draw the bounding box on the image
     
             w, h = x2-x1, y2-y1
             print(x1,y1,x2,y2)  # Print the coordinates of the bounding box
@@ -123,17 +128,16 @@ while True:
             cls =int(box.cls[0])
             currentClass = classNames[cls]
             
-            if currentClass =="car" or currentClass =="truck" or currentClass =="bus" \
-                or currentClass =="motorbike" and conf > 0.8:
-                cvzone.putTextRect(img,f'{currentClass} {conf}',(max(0,x1),max(35,y1)),scale = 0.6,thickness=1,offset=3)
-                cvzone.cornerRect(img,(x1,y1,w,h),l = 9,rt=5)
+            if currentClass == 'person' and conf > 0.3:
+                cvzone.putTextRect(img,f'{currentClass} {conf}',(max(0,x1),max(100,y1)),scale = 0.8,thickness=1,offset=3)
+                cvzone.cornerRect(img,(x1,y1,w,h),l = 10,rt=2)
                 #store array for tracking
                 currentArray = np.array([x1,y1,x2,y2,conf])
                 detections = np.vstack((detections,currentArray))
     
     resultsTracker = tracker.update(detections) 
-    cv2.line(img,(limit[0],limit[1]),(limit[2],limit[3]),(255,0,255),5)  # Draw a line on the image
-
+    cv2.line(img,(limitsUp[0],limitsUp[1]),(limitsUp[2],limitsUp[3]),(0,0,255),5)  # Draw a line on the image
+    cv2.line(img,(limitsDown[0],limitsDown[1]),(limitsDown[2],limitsDown[3]),(255,0,0),5)  # Draw a line on the image
     for result in resultsTracker:
         x1,y1,x2,y2,id = result
         x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
@@ -144,13 +148,18 @@ while True:
         
         cx,cy = (x1+w//2),(y1+h//2)  # Calculate the center of the bounding box
         cv2.circle(img,(cx,cy),5,(0,0,255),cv2.FILLED)
-        if limit[0] < cx < limit[2] and limit[1]-20 < cy < limit[1]+20:
-            if totalCount.count(id) ==0:
-                totalCount.append(id)
-            
-            
-        cvzone.putTextRect(img, f'Count : {len(set(totalCount))}', (50, 50))
+        if limitsUp[0] < cx < limitsUp[2] and limitsUp[1]-70 < cy < limitsUp[1]+70:
+            if totalCountUp.count(id) ==0:
+                totalCountUp.append(id)
+                cv2.line(img,(limitsUp[0],limitsUp[1]),(limitsUp[2],limitsUp[3]),(0,255,0),5)
         
+        if limitsDown[0] < cx < limitsDown[2] and limitsDown[1]-70 < cy < limitsDown[1]+70:
+            if totalCountDown.count(id) ==0:
+                totalCountDown.append(id)
+                cv2.line(img,(limitsDown[0],limitsDown[1]),(limitsDown[2],limitsDown[3]),(0,255,0),5)
+        # cvzone.putTextRect(img, f'Count : {len(set(totalCountUp))}', (50, 50))
+    cv2.putText(img,str(len(totalCountUp)),(255,100),cv2.FONT_HERSHEY_PLAIN,5,(139,195,75),5) 
+    cv2.putText(img,str(len(totalCountDown)),(400,100),cv2.FONT_HERSHEY_PLAIN,5,(250,0,0),5)    
     cv2.imshow("Image",img)  # Display the image in a window
     #cv2.imshow("Image region",imgRegion)  # Display the masked image in a window
     if cv2.waitKey(1) & 0xFF == ord('q'):  # Wait for 'q' key to exit
